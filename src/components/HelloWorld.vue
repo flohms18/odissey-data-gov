ec8e58b37956215281a48615f25fedbacf6edfea
flohms18 <trouilletflorent@outlook.com>
2026-02-25 15:43:23 +0100

    Responsive Design

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-6">
-                <h1>THE ODISSEY</h1>
+    <div class="container-fluid d-flex justify-content-center align-items-center vh-100">
+        <div class="row w-100 justify-content-center">
+            <div class="col-6 text-center">
+                <h1 class="title-odissey">THE ODISSEY</h1>
+                <p class="subtitle-odissey">The journey to data and AI governance</p>
             </div>
         </div>
     </div>
