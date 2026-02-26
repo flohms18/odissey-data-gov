74079a4c031fb3aa1d4b28bd19755497d3b9a5a1
flohms18 <trouilletflorent@outlook.com>
2026-02-26 03:58:46 +0100

    UPDATED

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <div class="row w-100 justify-content-center">
       <div class="col-6 text-center">
         <h1 class="title-odissey">THE ODISSEY</h1>
-        <p class="subtitle-odissey">The journey to data and AI governance</p>
+        <p class="subtitle-odissey">Journey to data and AI governance</p>
       </div>
     </div>
 
