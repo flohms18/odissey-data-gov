3b5450d063df0569d87f1ba84d174ca18435ff94
flohms18 <trouilletflorent@outlook.com>
2026-02-26 03:52:35 +0100

    New section added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,22 @@
 <template>
-    <div class="container-fluid d-flex justify-content-center align-items-center vh-100">
-        <div class="row w-100 justify-content-center">
-            <div class="col-6 text-center">
-                <h1 class="title-odissey">THE ODISSEY</h1>
-                <p class="subtitle-odissey">The journey to data and AI governance</p>
-            </div>
-        </div>
+  <div class="container-fluid d-flex justify-content-center align-items-center vh-100 position-relative">
+    <div class="row w-100 justify-content-center">
+      <div class="col-6 text-center">
+        <h1 class="title-odissey">THE ODISSEY</h1>
+        <p class="subtitle-odissey">The journey to data and AI governance</p>
+      </div>
     </div>
+
+    
+    <a href="#next-section" class="scroll-down-arrow"></a>
+  </div>
+
+  <section id="next-section" class="vh-100 bg-light d-flex justify-content-center align-items-center">
+    <h2>Next Section</h2>
+  </section>
 </template>
+
+<style scoped>
+
+
+</style>
