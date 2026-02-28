5f2123adc808d761cbf85e36adf8962eb4e50b9d
flohms18 <trouilletflorent@outlook.com>
2026-02-28 19:56:08 +0100

    Sections fixed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,16 @@
   </div>
 
   <section id="next-section" class="vh-100 d-flex justify-content-center align-items-center">
-    <h2 class="text-uppercase">Ulysses' journey as a metaphor for data governance</h2>
+    <div class="container-fluid d-flex justify-content-center align-items-center vh-100 position-relative">
+    <div class="row w-100 justify-content-center">
+      <div class="col-6 text-center">
+        <h1 class="title-odissey">THE ODISSEY</h1>
+        <p class="subtitle-odissey">Journey to data and AI governance</p>
+      </div>
+    </div>
+        <a href="#next-section" class="scroll-down-arrow"></a>
+
+    </div>
   </section>
 
   
