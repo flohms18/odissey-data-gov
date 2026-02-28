06dd85a09fba8c05795c1ac08d1f1c813765553d
flohms18 <trouilletflorent@outlook.com>
2026-02-28 19:47:01 +0100

    Games

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,16 +3,29 @@
     <div class="row w-100 justify-content-center">
       <div class="col-6 text-center">
         <h1 class="title-odissey">THE ODISSEY</h1>
-        <p class="subtitle-odissey">Journey to data and AI governance</p>
+        <p class="subtitle-odissey">Journey to efficient and robust data and AI governance</p>
       </div>
     </div>
 
     
-    <a href="#next-section" class="scroll-down-arrow"></a>
+    <a href="#second-section" class="scroll-down-arrow"></a>
   </div>
 
-  <section id="next-section" class="vh-100 bg-light d-flex justify-content-center align-items-center">
-    <h2>Next Section</h2>
+  <section id="second-section" class="vh-100 d-flex justify-content-center align-items-center">
+    <h2 class="text-uppercase second-subtitle fw-bold">Ulysses' journey as a metaphor for data governance</h2>
+        <a href="#third-section" class="scroll-down-arrow"></a>
+
+  </section>
+
+  <section id="third-section" class="vh-100 d-flex justify-content-center align-items-center">
+    <h2 class="text-uppercase third-subtitle">Ulysses' journey as a metaphor for data governance</h2>
+            <a href="#fourth-section" class="scroll-down-arrow"></a>
+
+  </section>
+  
+  <section id="fourth-section" class="vh-100 d-flex justify-content-center align-items-center">
+    <h2 class="text-uppercase third-subtitle">Ulysses' journey as a metaphor for data governance</h2>
+    
   </section>
 </template>
 
